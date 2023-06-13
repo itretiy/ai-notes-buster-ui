4854e637fb9586045f05d0db4cc90003edd3b985
Oleksandr Tretiakov <oleksandr.tretiakov@zapier.com>
2023-06-13 11:14:29 +0200

    Fix

--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -1,14 +1,15 @@
-import { TranslateBody } from '@/types/types';
+import { GenerateBody } from '@/types/types';
 import { OpenAIStream } from '@/utils';
 
 export const config = {
   runtime: 'edge',
 };
 
+// TODO remove?
 const handler = async (req: Request): Promise<Response> => {
   try {
     const { inputLanguage, outputLanguage, inputCode, model, apiKey } =
-      (await req.json()) as TranslateBody;
+      (await req.json()) as GenerateBody;
 
     const stream = await OpenAIStream(
       inputLanguage,
